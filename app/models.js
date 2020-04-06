735bf50929e5d6b08fa88606716c47d577e1b63a
Larry <larry@esportstickets.gg>
2020-04-06 13:57:12 -0400

    boilerplate for models

---
+++ app/models.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../db');
+
+// class Table extends Sequelize.Model {}
+// Table.init(
+//     {
+//         id: {
+//             type: Sequelize.UUID,
+//             primaryKey: true,
+//         },
+//     },
+//     {
+//         sequelize,
+//     },
+// );
